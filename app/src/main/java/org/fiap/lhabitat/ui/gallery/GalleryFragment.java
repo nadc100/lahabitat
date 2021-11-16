37b42ddf2141c8890d4e1bdffd549c957b67d88d
JjDaBid <jaimedavidp@hotmail.com>
2021-11-16 00:05:37 -0500

    Fragment Property View. Layout Designed

--- app/src/main/java/org/fiap/lhabitat/ui/gallery/GalleryFragment.java
+++ app/src/main/java/org/fiap/lhabitat/ui/gallery/GalleryFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -28,7 +29,7 @@ public class GalleryFragment extends Fragment {
         binding = FragmentGalleryBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textGallery;
+        final TextView textView = binding.title;
         galleryViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
